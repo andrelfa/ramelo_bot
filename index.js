70e3acbed686970774f556739fc5f16262f85d3d
Andre Luiz Ferreira Antunes <andrelfantunes@gmail.com>
2019-01-16 22:35:19 -0200

    Completo

--- index.js
+++ index.js
@@ -3,43 +3,168 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const config = require("./auth.json");
+const youtube = require('./youtube.js');  
+const ytdl = require('ytdl-core');
 
-client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+let voiceChannel = null;
+let ytAudioQueue = [];
+let dispatcher = null;
+
+client.on('ready', function () {
+  console.log('I am ready');
 });
 
-client.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+client.on('message', function (message) {
+  var messageParts = message.content.split(' ');
+
+  var command = messageParts[0].toLowerCase();
+  var parameters = messageParts.splice(1, messageParts.length);
+
+  console.log("command: " + command);
+  console.log("parameters: " + parameters);
+
+  switch (command) {
+      case "!netinha":
+          PlayCommand(parameters.join(" "), message);
+          break;
+  }
 });
 
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+/* COMMAND HANDLERS */
 
+/// lists out all of the bot commands
+function HelpCommand(originalMessage) {
+  originalMessage.reply("*join <channel-to-join> - Connects to bot to a channel by channel name");
+  originalMessage.reply("*play <YouTube search term> - Plays audio from YouTube based on the search term");
+  originalMessage.reply("*playqueue - Lists the audio remaining in the play queue");
+}
 
-client.on("message", async message => {
-  
-  if(message.author.bot) return;
-  
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
+/// plays audio based on results from youtube search
+function PlayCommand(searchTerm) {
 
-  // const voiceChannel = message.member.voiceChannel;
-  // if (!voiceChannel){
-  //     return message.channel.sendMessage("you must be in a voice channel to request me");
-  // }  
-  
-  if(command === "netinha") {
-    const m = await message.channel.send(`teste`);
-    message.member.voiceChannel.join();
+  // if not connected to a voice channel then connect to first one
+  if (client.voiceConnections.array().length == 0) {
+      var defaultVoiceChannel = client.channels.find(val => val.type === 'voice').name;
+      JoinCommand(defaultVoiceChannel);
   }
 
-});
+  // search youtube using the given search search term and perform callback action if video is found
+  
+  youtube.search(searchTerm, QueueYtAudioStream);
+}
+
+/// lists out all music queued to play
+function PlayQueueCommand(message) {
+  var queueString = "";
+
+  for(var x = 0; x < ytAudioQueue.length; x++) {
+      queueString += ytAudioQueue[x].videoName + ", ";
+  }
+
+  queueString = queueString.substring(0, queueString.length - 2);
+  message.reply(queueString);
+}
+
+/// joins the bot to the specified voice channel
+function JoinCommand(channelName) {
+  var voiceChannel = GetChannelByName(channelName);
+
+  if (voiceChannel) {
+      voiceChannel.join();
+      console.log("Joined " + voiceChannel.name);
+  }
+
+  return voiceChannel;
+}
+
+/* END COMMAND HANDLERS */
+/*----------------------------------------------------------------------*/
+/* HELPER METHODS */
+
+/// returns the channel that matches the name provided
+function GetChannelByName(name) {
+  var channel = client.channels.find(val => val.name === name);
+  return channel;
+}
+
+/// Queues result of Youtube search into stream
+function QueueYtAudioStream(videoId, videoName) {
+  var streamUrl = `${youtube.watchVideoUrl}${videoId}`;
+
+  if (!ytAudioQueue.length) {
+      ytAudioQueue.push(
+          {
+              'streamUrl': 'https://www.youtube.com/watch?v=6WlaauPk1g8&feature=youtu.be',
+              'videoName': videoName
+          }
+      );
+
+      console.log("Queued audio " + streamUrl);
+      PlayStream(ytAudioQueue[0].streamUrl);
+  }
+  else {
+      ytAudioQueue.push(
+          {
+              'streamUrl': 'https://www.youtube.com/watch?v=6WlaauPk1g8&feature=youtu.be',
+              'videoName': videoName
+          }
+      );
+
+      console.log("Queued audio " + videoName);
+  }
+
+}
+
+/// Plays a given stream
+function PlayStream(streamUrl) {
+
+  const streamOptions = {seek: 0, volume: 1};
+
+  if (streamUrl) {
+      const options = {
+          filter: 'video', 
+          begin: '20s'
+      }
+      const stream = ytdl(streamUrl, options);
+
+      if (dispatcher == null) {
+
+          var voiceConnection = client.voiceConnections.first();
+          //console.log(voiceConnection);
+
+          if (voiceConnection) {
+
+              console.log("Now Playing " + ytAudioQueue[0].videoName);
+              dispatcher = client.voiceConnections.first().playStream(stream, streamOptions);
+
+              dispatcher.on('end', () => {                       
+                dispatcher = null;                 
+                PlayNextStreamInQueue();
+              })
+
+              dispatcher.on('error', (err) => {
+                  console.log(err);
+              });
+          }
+      }
+      else {
+        console.log('entrou aqui')
+          dispatcher = client.voiceConnections.first().playStream(stream, streamOptions);
+      }
+  }
+}
+
+/// Plays the next stream in the queue
+function PlayNextStreamInQueue() {
+
+  ytAudioQueue.splice(0, 1);
+
+  // if there are streams remaining in the queue then try to play
+  if (ytAudioQueue.length != 0) {
+      console.log("Now Playing " + ytAudioQueue[0].videoName);
+      PlayStream(ytAudioQueue[0].streamUrl);
+  }
+}
+/* END HELPER METHODS */
 
 client.login(config.token);
